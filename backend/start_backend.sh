13728ada6ac177bcbb592a8be7951e1f8bb4ac33
Bartosz Kubek <kubek.bartosz@gmail.com>
2024-09-16 08:13:57 +0200

    - second version of fix for intitial db loading

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+echo "Starting backend..."
+
 cd /app/upfront_backend || exit
 
 chmod +x bin/*
 chmod +x *.sh
 composer install
-
-# Wait for the database to be ready
-sleep 15
 
 ./bin/console doctrine:database:drop --force --no-interaction
 ./bin/console doctrine:database:create
